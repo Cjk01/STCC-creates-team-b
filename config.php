6b9b33b05a310bf7fc4d60fd75cb064b979738fa
TheBeast <coddude21@hotmail.com>
2020-09-12 14:19:41 -0400

    Added a login session

---
+++ config.php
@@ -0,0 +1,7 @@
+<?php
+define('DB_SERVER', 'thebeast.pro');
+define('DB_USERNAME', 'thebeast_stccsoc');
+define('DB_PASSWORD', 'stccsocial');
+define('DB_DATABASE', 'thebeast_users_stccsocial');
+$db = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
+?>
--- login.php
+++ login.php
@@ -1,9 +1,34 @@
 <?php
-//Verify info here
+include("config.php");
+session_start();
+if($_SERVER["REQUEST_METHOD"] == "POST") {
+    // username and password sent from form
+
+    $myusername = mysqli_real_escape_string($db,$_POST['Email']);
+    $mypassword = mysqli_real_escape_string($db,$_POST['password']);
+
+    $sql = "SELECT email FROM users WHERE email = '$myusername' and password = MD5('$mypassword')";
+    $result = mysqli_query($db,$sql);
+    $row = mysqli_fetch_array($result,MYSQLI_ASSOC);
+    $active = $row['active'];
+    session_id($_SESSION['Email']);
+
+    $count = mysqli_num_rows($result);
+
+    // If result matched $myusername and $mypassword, table row must be 1 row
+
+    if(!isset($_SESSION['uname'])){
+        header('Location: home/');
+
+    }
+}
 ?>
 <!DOCTYPE HTML>
 <head>
     <link rel="stylesheet" type="text/css" href="assets/css/style.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+    <link rel="stylesheet" href="https://code.getmdl.io/1.3.0/material.red-blue.min.css" />
+    <script defer src="https://code.getmdl.io/1.3.0/material.min.js"></script>
     <script src="assets/js/main.js" type="text/javascript"></script>
     <script src="assets/js/customLinks.js" type="text/javascript"></script>
     <script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>    <title>Login</title>
@@ -11,20 +36,24 @@
 <hgroup>
     <h1>STCC Social Media </h1>
 </hgroup>
-<form>
+<form action="" method="post">
     <div class="group">
-        <input type="text" id="uEmail"><span class="highlight"></span><span class="bar"></span>
-        <label>Email</label>
+        <div class="mdl-textfield mdl-js-textfield">
+            <input class="mdl-textfield__input" type="text" id="Email" name="Email">
+            <label class="mdl-textfield__label" for="Email">Email</label>
+        </div>
     </div>
     <div class="group">
-        <input type="password" id="uPass"><span class="highlight"></span><span class="bar"></span>
-        <label>Password</label>
+        <div class="mdl-textfield mdl-js-textfield">
+            <input class="mdl-textfield__input" type="password" id="password" name="password">
+            <label class="mdl-textfield__label" for="password">Password</label>
+        </div>
     </div>
-    <button type="button" class="button buttonBlue" onclick="userLogin()">Login
-        <div class="ripples buttonRipples"><span class="ripplesCircle"></span></div>
+    <button class="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect mdl-button--accent" type="submit">
+        Login
     </button>
-    <button type="button" class="button buttonBlue" id="signup" onclick="userRegister();">Sign up
-        <div class="ripples buttonRipples"><span class="ripplesCircle"></span></div>
+    <button class="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect mdl-button--accent" type="button" onclick="userRegister()">
+        Sign up
     </button>
 </form>
 <footer>
