9a899fa7557814c7cb7a06b4a0728fa7f454e303
Craig <47963766+Cjk01@users.noreply.github.com>
2020-09-12 15:25:54 -0400

    changed design

--- register.php
+++ register.php
@@ -4,6 +4,9 @@
 <!DOCTYPE HTML>
 <head>
     <link rel="stylesheet" type="text/css" href="assets/css/style.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+    <link rel="stylesheet" href="https://code.getmdl.io/1.3.0/material.red-blue.min.css" />
+    <script defer src="https://code.getmdl.io/1.3.0/material.min.js"></script>
     <script src="assets/js/main.js" type="text/javascript"></script>
     <script src="assets/js/customLinks.js" type="text/javascript"></script>
     <script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>    <title>Login</title>
@@ -11,26 +14,36 @@
 <hgroup>
     <h1>STCC Social Media </h1>
 </hgroup>
-<form>
-    <div class="group">
-        <input type="text" id="firstName"><span class="highlight"></span><span class="bar"></span>
-        <label>First Name</label>
+<form action="" method="post">
+<div class="group">
+        <div class="mdl-textfield mdl-js-textfield">
+            <input class="mdl-textfield__input" type="text" id="Email" name="First Name">
+            <label class="mdl-textfield__label" for="password">First Name</label>
+        </div>
     </div>
     <div class="group">
-        <input type="text" id="lastName"><span class="highlight"></span><span class="bar"></span>
-        <label>Last Name</label>
+        <div class="mdl-textfield mdl-js-textfield">
+            <input class="mdl-textfield__input" type="text" id="Email" name="Last Name">
+            <label class="mdl-textfield__label" for="password">Last Name</label>
+        </div>
     </div>
     <div class="group">
-        <input type="text" id="uEmail"><span class="highlight"></span><span class="bar"></span>
-        <label>Email</label>
+        <div class="mdl-textfield mdl-js-textfield">
+            <input class="mdl-textfield__input" type="text" id="Email" name="Email">
+            <label class="mdl-textfield__label" for="Email">Email</label>
+        </div>
     </div>
     <div class="group">
-        <input type="password" id="uPass"><span class="highlight"></span><span class="bar"></span>
-        <label>password</label>
+        <div class="mdl-textfield mdl-js-textfield">
+            <input class="mdl-textfield__input" type="password" id="password" name="password">
+            <label class="mdl-textfield__label" for="password">Password</label>
+        </div>
     </div>
-    <button type="button" class="button buttonBlue" onclick="userLogin();">Submit
-        <div class="ripples buttonRipples"><span class="ripplesCircle"></span></div>
+    
+    <button class="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect mdl-button--accent" type="submit">
+        Create Account
     </button>
+    
 </form>
 <footer>
     <img src="assets/images/Springfield_Technical_Community_College_(STCC)_seal.png" alt="STCC Logo">
